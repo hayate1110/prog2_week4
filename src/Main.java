6ada8d023b8f684cc6feb6fde1e45904dbb92125
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2022-11-01 09:32:17 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main method runs");
+        Chatbot bot1 = new Chatbot();
+        Chatbot bot2 = new Chatbot("mybot");
+        bot1.greeting();
+        bot2.greeting();
+        bot2.setName("差別用語");
+        bot2.setName("rename");
+        bot2.greeting();
+    }
+}
